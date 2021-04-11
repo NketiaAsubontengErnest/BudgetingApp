84aee7aa438b12840d4b65fe1ac53fc96e640b92
Ernest Nketia Asubonteng <67163320+NketiaAsubontengErnest@users.noreply.github.com>
2021-04-11 05:00:21 -0700

    Add files via upload
    some small changes and addition

--- BudgetingApp.py
+++ BudgetingApp.py
@@ -27,7 +27,7 @@ def Transfer():
     check = True
     while (check == True):
         WhatToDo = int(input(
-            "What Operation do you want to perform:\n1) Transfer from food to clothings Account  \n2) Transfer from food to entertainment Account \n3) Transfer from clothing to food Account \n4) Transfer from clothing to entertainment Account \n5) Transfer from entertainment to food Account \n6) Transfer from entertainment to clothings Account\n0)Back \nType here: "))
+            "What Operation do you want to perform:\n1) Transfer from food to clothings Account  \n2) Transfer from food to entertainment Account \n3) Transfer from clothing to food Account \n4) Transfer from clothing to entertainment Account \n5) Transfer from entertainment to food Account \n6) Transfer from entertainment to clothings Account\n7) Exit Program \n0)Back \nType here: "))
 
         if WhatToDo == 1:
             amount = int(input("Enter your money: "))
@@ -67,6 +67,8 @@ def Transfer():
                 f'Your food Balance is now: GH₵{entertainmentAcc} and your clothing Balance is: GH₵{clothingAcc}')
         elif (WhatToDo == 0):
             break
+        elif(WhatToDo == 7):
+            exit()
         else:
             print("Invalid Option!")
             count += 1
@@ -80,11 +82,11 @@ check = True
 print("\n\n==================================== Welcome to Bugeting System ====================================\n")
 while (check == True):
     WhatToDo = int(
-        input("\nWhat Operation do you want to perform:\n1) Deposit \n2) Withdraw \n3) Check Balance \n4) Transfers \n5) Exit\nType here: \n"))
+        input("\nWhat Operation do you want to perform:\n1) Deposit \n2) Withdraw \n3) Check Balance \n4) Transfers \n5) Exit Program\nType here: \n"))
     if (WhatToDo == 1):
         while (check == True):
             WhatToDo = int(input(
-                "What Operation do you want to perform:\n1) Deposit to food Balance  \n2) Deposit to clothing Balance \n3) Deposit to entertainment Balance\n0)Back\n Type here:  "))
+                "What Operation do you want to perform:\n1) Deposit to food Balance  \n2) Deposit to clothing Balance \n3) Deposit to entertainment Balance\n4) Exit Program\n0)Back\nType here:  "))
             if (WhatToDo == 1):
                 amount = int(input("Enter your money: "))
                 foodAcc = food.deposit(amount)
@@ -99,6 +101,8 @@ while (check == True):
                 print("Your entertainment balance is: GH₵", entertainmentAcc)
             elif(WhatToDo == 0):
                 break
+            elif(WhatToDo == 4):
+                exit()
             else:
                 print("Invalid Option!")
                 count += 1
@@ -111,7 +115,7 @@ while (check == True):
     elif (WhatToDo == 2):
         while (check == True):
             WhatToDo = int(input(
-                "What Operation do you want to perform:\n1) Withdraw from food Balance  \n2) Withdraw from clothing Balance \n3) Withdraw from entertainment Balance \n0)Back \nType here: "))
+                "What Operation do you want to perform:\n1) Withdraw from food Balance  \n2) Withdraw from clothing Balance \n3) Withdraw from entertainment Balance\n4) Exit Program \n0) Back \nType here: "))
             if (WhatToDo == 1):
                 amount = float(input("Enter your money: "))
                 foodAcc = food.withdraw(amount)
@@ -126,6 +130,8 @@ while (check == True):
                 print("Your entertainment balance is: GH₵", entertainmentAcc)
             elif (WhatToDo == 0):
                 break
+            elif(WhatToDo == 4):
+                exit()
             else:
                 print("Invalid Option!")
                 count += 1
@@ -137,7 +143,7 @@ while (check == True):
     elif (WhatToDo == 3):
         while (check == True):
             WhatToDo = int(input(
-                "What Operation do you want to perform:\n1) Check food Balance  \n2) Check clothing Balance \n3) Check entertainment Balance \n0)Back \nType here: "))
+                "What Operation do you want to perform:\n1) Check food Balance  \n2) Check clothing Balance \n3) Check entertainment Balance\n4) Exit Program \n0) Back \nType here: "))
             if (WhatToDo == 1):
                 foodAcc = food.checkBalance()
                 print("Your food Balance is: GH₵", foodAcc)
@@ -149,6 +155,8 @@ while (check == True):
                 print("Your entertainment Balance is: GH₵", entertainmentAcc)
             elif(WhatToDo == 0):
                 break
+            elif(WhatToDo == 4):
+                exit()
             else:
                 print("Invalid Option!")
                 count += 1
